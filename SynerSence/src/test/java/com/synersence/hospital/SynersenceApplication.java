95a7cd7384040e9bdbc5c4bfdc63521f74116324
Pankti0911 <pankti112004@gmail.com>
2026-01-09 13:11:52 +0530

    Delete SynerSence directory

--- SynerSence/src/test/java/com/synersence/hospital/SynersenceApplication.java
+++
@@ -1,9 +0,0 @@
-package com.synersence.hospital;
-
-import org.springframework.boot.SpringApplication;
-
-public class SynersenceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SynersenceApplication.class, args);
-    }
-}
